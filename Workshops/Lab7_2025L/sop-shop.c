37647505d82a75285257ccb62c28da9463fb8a84
uasuna2022 <max.gomanchuk@gmail.com>
2026-04-15 17:24:17 +0200

    OPS2 - Lab7_2025L stage 2 completed

--- Workshops/Lab7_2025L/sop-shop.c
+++ Workshops/Lab7_2025L/sop-shop.c
@@ -73,6 +73,7 @@ void print_array(int* array, int n)
 
 int main(int argc, char** argv) 
 {
+    srand(time(NULL));
     if (argc != 3)
         usage(argv[0]);
     
@@ -86,7 +87,7 @@ int main(int argc, char** argv)
     if ((shm_fd = shm_open(SHOP_FILENAME, O_CREAT | O_TRUNC | O_RDWR, 0600)) == -1)
         ERR("shm_open");
     
-    int max_size = sizeof(int) * MAX_SHELVES;
+    int max_size = sizeof(int) * MAX_SHELVES + sizeof(pthread_mutex_t) * MAX_SHELVES;
     if (ftruncate(shm_fd, max_size))
         ERR("ftruncate");
     
@@ -95,12 +96,93 @@ int main(int argc, char** argv)
         ERR("mmap");
 
     int* start_arr = (int*)addr;
+    pthread_mutex_t* mutexes_start = (pthread_mutex_t*)(start_arr + N);
+
+    pthread_mutexattr_t mutex_attributes;
+    if (pthread_mutexattr_init(&mutex_attributes))
+        ERR("pthread_mutexattr_init");
+    if (pthread_mutexattr_setpshared(&mutex_attributes, PTHREAD_PROCESS_SHARED))
+        ERR("pthread_mutexattr_setpshared");
+    if (pthread_mutexattr_setrobust(&mutex_attributes, PTHREAD_MUTEX_ROBUST))
+        ERR("pthread_mutexattr_setrobust");
+    
         
     for (int i = 0; i < N; i++)
+    {
         start_arr[i] = i + 1;
+        if (pthread_mutex_init(mutexes_start + i, &mutex_attributes))
+            ERR("pthread_mutex_init");
+    }
+    if (pthread_mutexattr_destroy(&mutex_attributes))
+        ERR("pthread_mutexattr_destroy");
 
     shuffle(start_arr, N);
     print_array(start_arr, N);
+
+    pid_t* worker_pids = (pid_t*)malloc(sizeof(pid_t) * M);
+    if (!worker_pids) 
+        ERR("malloc");
+
+    for (int i = 0; i < M; i++)
+    {
+        *(worker_pids + i) = fork();
+        if (worker_pids[i] == -1)
+            ERR("fork");
+        if (worker_pids[i] == 0)
+        {
+            printf("[%d] Worker reports for a night shift\n", getpid());
+            srand(time(NULL) ^ getpid());
+            int random_idx_less, random_idx_more;
+
+            for (int j = 0; j < 10; j++)
+            {
+                while (1)
+                {
+                    random_idx_less = rand() % N;
+                    random_idx_more = rand() % N;
+                    printf("[%d] idx1 - %d idx2 - %d\n", getpid(), random_idx_less, random_idx_more);
+                    if (random_idx_less != random_idx_more)
+                        break;
+                }
+
+                if (random_idx_less > random_idx_more)
+                    swap(&random_idx_less, &random_idx_more);
+
+                if (pthread_mutex_lock(mutexes_start + random_idx_less))
+                    ERR("pthread_mutex_lock");
+                if (pthread_mutex_lock(mutexes_start + random_idx_more))
+                    ERR("pthread_mutex_lock");
+                
+                if (start_arr[random_idx_less] > start_arr[random_idx_more])
+                    swap(&start_arr[random_idx_less], &start_arr[random_idx_more]);
+
+                if (pthread_mutex_unlock(mutexes_start + random_idx_less))
+                    ERR("pthread_mutex_unlock");
+                if (pthread_mutex_unlock(mutexes_start + random_idx_more))
+                    ERR("pthread_mutex_unlock");
+
+                ms_sleep(100);
+            }
+
+            free(worker_pids);
+            if (munmap(addr, max_size))
+                ERR("munmap");
+            close(shm_fd);
+
+            exit(EXIT_SUCCESS);
+        }
+    }
+
+    while (wait(NULL) > 0) {}
+
+    printf("Night shift in Bitronka is over\n");
+    print_array(start_arr, N);
+
+    for (int i = 0; i < N; i++)
+    {
+        if (pthread_mutex_destroy(mutexes_start + i))
+            ERR("pthread_mutex_destroy");
+    }
     
     if (munmap(addr, max_size))
         ERR("munmap");
